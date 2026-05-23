529d337d5fd099a629ae23f79bb170b76c87b90d
anisaramanda48 <anisaramanda48@gmail.com>
2026-05-23 10:10:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-# Hirarki teks
+st.set_page_config(
+  page_title="Finance Dashboard",
+  layout="wide"
+)
+
 st.title("📊 Dashboard")
 st.header("Laporan Bulanan")
 st.subheader("📈 Monthly Expenses")
