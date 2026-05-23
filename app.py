536067eaf2499edf87b0a8a392cc1fc6f7d7cd79
anisaramanda48 <anisaramanda48@gmail.com>
2026-05-23 10:21:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
 st.set_page_config(
-  page_title="Finance Dashboard",
-  layout="wide"
+  page_title="Kuliah Praktisi 2305",
+  page_icon="🧊",
+  layout="centered",
+  initial_sidebar_state="expanded"
 )
 
 st.title("📊 Dashboard")
 st.header("Laporan Bulanan")
 st.subheader("📈 Monthly Expenses")
 st.caption("Made with ❤️ using Streamlit")
-
+st.write("Hello, *World!* ")
